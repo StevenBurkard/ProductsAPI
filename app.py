362860a9421d781db0a2782e156cf65cc79bbd64
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-06-19 11:02:48 -0500

    updated app.py file to create model for product_api table

--- app.py
+++ app.py
@@ -23,8 +23,15 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Float(8, 2), nullable = False)
+    inventory_quantity = db.Column(db.Integer, nullable = False)
 
-
+    def __repr__(self):
+        return f'{self.name} {self.description} {self.price} {self.inventory_quantity}'
 
 # Schemas
 
